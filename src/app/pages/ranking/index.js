280acc2e29608e06a72fe7954fc2196b0bde75ad
Adrian <adriansb3105@gmail.com>
2017-12-03 14:40:34 -0600

    Minor changes

--- src/app/pages/ranking/index.js
+++ src/app/pages/ranking/index.js
@@ -36,10 +36,6 @@ class Ranking extends Component {
     }
 
     render() {
-         if (!getAuthenticatedUser) {
-            return <Redirect to='/'/>;
-        }
-
         return(
             <div>
                 <Header />
