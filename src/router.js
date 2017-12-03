941335f6af230fdf3b250f34ea4490b087ef1391
Adrian <adriansb3105@gmail.com>
2017-12-03 14:43:30 -0600

    Before asking for a path, it verifies if the user is authenticated, or go to Home instead

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { getAuthenticatedUser } from './store';
 import Home from './app/pages/home/index';
 import Dashboard from './app/components/Dashboard';
 import Header from './app/components/Header';
@@ -9,21 +10,24 @@ import Assault from './app/pages/assaults/index';
 import Settings from './app/pages/settings/index';
 
 const Routes = (props) => {
-    return (
-        <Router>
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/dashboard" component={Dashboard}/>
-                    <Route path="/profile" component={Profile}/>
-                    <Route path="/ranking" component={Ranking}/>
-                    <Route path="/settings" component={Settings}/>
-                    <Route path="/assault_report/:targetId" component={Assault}/>
-                </Switch>
-            </div>
-        </Router>
-    );
+	let defaultComponent = Home;
+	let isAuth = getAuthenticatedUser();
+
+	return (
+		<Router>
+			<div>
+				<Header />
+				<Switch>
+					<Route exact path="/" component={ isAuth? Dashboard : defaultComponent }/>
+					<Route path="/profile" component={ isAuth? Profile  : defaultComponent }/>
+					<Route path="/ranking" component={ isAuth? Ranking  : defaultComponent }/>
+					<Route path="/settings" component={ isAuth? Settings : defaultComponent }/>
+					<Route path="/assault_report/:targetId" component={ isAuth? Assault : defaultComponent }/>
+					<Route component={ isAuth? Dashboard : defaultComponent }/>
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default Routes;
