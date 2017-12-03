abddf40d7019aff2926337b5af8e4a7c7d792cc9
Adrian <adriansb3105@gmail.com>
2017-12-03 02:03:46 -0600

    Now it knows if the parameter is an email or an username with a regular expression and post the data depends on the type of it

--- src/app/services/api/Auth.js
+++ src/app/services/api/Auth.js
@@ -5,11 +5,17 @@ const LOGIN_URL = BASE_URL + '/login'; //TODO: create this route in web service
 const SIGN_UP_URL = BASE_URL + '/create_guerrilla';
 
 const signUp = (data) => {
-    return post(SIGN_UP_URL, data);
+    return axios.post(SIGN_UP_URL, data);
 };
 
-const login = (email) => {
-    return post(LOGIN_URL, {email: email});
+const login = (data) => {
+	let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+	if(regex.test(data)){
+		return post(LOGIN_URL, {email: data});
+	}else{
+		return post(LOGIN_URL, {username: data});
+	}
 };
 
 const post = (url, data) => {
@@ -19,8 +25,8 @@ const post = (url, data) => {
             Promise.resolve(response.data)
         ))
         .catch((errors) => (
-            Promise.reject(errors.response)
+            Promise.reject(errors)
         ));
 };
 
-export {signUp, login};
+export { signUp, login };
