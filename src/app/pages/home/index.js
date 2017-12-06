01903d024bf76ba828bf3b22f0a9be37f93ab83d
Bryan <bryan.moralessanchez@ucr.ac.cr>
2017-12-06 12:30:10 -0600

    Added the new style of the home view

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -32,10 +32,10 @@ class Home extends Component {
                     <div>
                         <ul className="nav nav-pills justify-content-center mt-3 mb-3">
                             <li className="nav-item active mr-5">
-                                <a data-toggle="tab" href="#login">Login</a>
+                                <a class="text-white" data-toggle="tab" href="#login">Login</a>
                             </li>
                             <li className="nav-item">
-                                <a data-toggle="tab" href="#sign">Sign</a>
+                                <a class="text-white" data-toggle="tab" href="#sign">Sign</a>
                             </li>
                         </ul>
                         <div className="tab-content">
@@ -56,9 +56,41 @@ class Home extends Component {
 const HomeContainer = (props) => {
     return (
         <div className="container">
-            <h1 className="mx-auto text-center mt-5 display-4">Welcome to Guerrilla Game</h1>
             <div className="row">
+                <div className="col-md-8">
+                    <section class="guerrilla">
+                        <div class="content">
+                            <div class="title">
+                                <p>Welcome</p>
+                                <h1>Guerrilla Game Web</h1>
+                            </div>
+                            <p class="py-5">
+                                En un mundo paralelo, donde la crisis
+                                político-social ha alcanzado niveles más allá de
+                                los nuestros, los gobiernos han sucumbido ante el
+                                poder de las grandes corporaciones.
+                            </p>
+                            <p class="py-5">
+                                La democracia y las misiones diplomáticas han
+                                fallado y ahora estas corporaciones subcontratan
+                                guerrillas para zanjar sus diferencias.
+                            </p>
+                            <p class="py-5">
+                                El mundo entero se ha vuelto un campo de 
+                                batalla a favor del interés de unos pocos.
+                            </p>
+                            <ul class="text-center">
+                                <h1>Desarrollado por:</h1>
+                                <li>Daniel Castillo Leiva</li>
+                                <li>Adrián Serrano Brenes</li>
+                                <li>Priscilla Mena</li>
+                                <li>Bryan Morales Sánchez</li>
+                            </ul>
+                        </div>
+                    </section>
+                </div>
                 <div className="col-md-4 mx-auto col-sm-8 col-xs-12">
+                    <h1 className="mx-auto text-center mt-5 display-4 text-white">Welcome to Guerrilla Game</h1>
                     {props.children}
                 </div>
             </div>
